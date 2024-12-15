5287dc0ecdf060cd09071133868213c9d33b41e0
Jeje <jjh4450jjh4450@gmail.com>
2024-12-15 20:41:20 +0900

    first init

---
+++ android/app/src/main/kotlin/jeje/work/checkmate/MainActivity.kt
@@ -0,0 +1,5 @@
+package jeje.work.checkmate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/auth_data.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:checkmate/data/firestor.dart';
+
+class AuthenticationRemote{
+  Future<void> login(String email, String password) async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email.trim(), password: password.trim());
+  }
+
+  Future<void> register(
+      String email, String password, String PasswordConfirm) async {
+    if (PasswordConfirm == password) {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: email.trim(), password: password.trim())
+          .then((value) {
+        Firestore_Datasource().CreateUser(email);
+      });
+    }
+  }
+}
